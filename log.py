f7add1325a45b790ec07f380656ada6af8da5b24
Aztec Rabbit <ars.xda@gmail.com>
2019-12-07 00:46:15 +0700

    ...

--- log.py
+++ log.py
@@ -13,17 +13,17 @@ class log(object):
         self.libutils = utils(__file__)
         self.lock = self.libutils.lock
 
-        self.patterns = {
-            'CC' : '\033[0m',    'BB' : '\033[1m',
-            'D1' : '\033[30;1m', 'D2' : '\033[30;2m',
-            'R1' : '\033[31;1m', 'R2' : '\033[31;2m',
-            'G1' : '\033[32;1m', 'G2' : '\033[32;2m',
-            'Y1' : '\033[33;1m', 'Y2' : '\033[33;2m',
-            'B1' : '\033[34;1m', 'B2' : '\033[34;2m',
-            'P1' : '\033[35;1m', 'P2' : '\033[35;2m',
-            'C1' : '\033[36;1m', 'C2' : '\033[36;2m',
-            'W1' : '\033[37;1m', 'W2' : '\033[37;2m',
-        }
+        self.patterns = [
+            ('CC', '\033[0m'),    ('BB', '\033[1m'),
+            ('D1', '\033[30;1m'), ('D2', '\033[30;2m'),
+            ('R1', '\033[31;1m'), ('R2', '\033[31;2m'),
+            ('G1', '\033[32;1m'), ('G2', '\033[32;2m'),
+            ('Y1', '\033[33;1m'), ('Y2', '\033[33;2m'),
+            ('B1', '\033[34;1m'), ('B2', '\033[34;2m'),
+            ('P1', '\033[35;1m'), ('P2', '\033[35;2m'),
+            ('C1', '\033[36;1m'), ('C2', '\033[36;2m'),
+            ('W1', '\033[37;1m'), ('W2', '\033[37;2m'),
+        ]
 
         self.type = 1
         self.prefix = ''
@@ -66,7 +66,7 @@ class log(object):
         self.log(value, prefix=prefix, suffix=suffix, value_prefix=value_prefix, value_suffix=value_suffix, color=color, type=type)
 
     def log_replace(self, value, color='[G1]'):
-        terminal_columns = self.libutils.terminal_size()['columns']
+        terminal_columns = self.libutils.get_terminal_size()['columns']
         value = value[:terminal_columns-3] + '...' if len(value) > terminal_columns else value
         value = self.libutils.colors(f'{color}{value}', self.patterns)
         with self.lock:
